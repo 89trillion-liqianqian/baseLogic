ee2e081373beaec1de2ea439df2f0cc4da77e906
liqianqian <liqianqian@89trillion.com>
2021-07-27 17:36:45 +0800

    删除多余代码

--- internal/ctrl/base.go
+++ internal/ctrl/base.go
@@ -5,7 +5,6 @@ import (
 	"ginserver/internal/handler"
 	"ginserver/internal/myerr"
 	"github.com/gin-gonic/gin"
-	"strconv"
 )
 
 // ping
@@ -20,7 +19,7 @@ func PingFunc(c *gin.Context) {
 func GetIntByStr(c *gin.Context) {
 	str := c.PostForm("str")
 	if len(str) < 1 {
-		msg := "参数不正确"
+		msg := "参数为空"
 		myerr.ResponseErr(c, msg)
 		return
 	}
@@ -36,25 +35,4 @@ func GetIntByStr(c *gin.Context) {
 		"data": reData,
 	})
 	return
-}
-
-// 校验字符串值合法性
-func CheckStr(str string) bool {
-	strArr := []rune(str)
-	lenStr := len(strArr)
-	for k := 0; k < lenStr; k++ {
-		strV := string(strArr[k])
-		if strV == "+" || strV == "-" || strV == "*" || strV == "/" {
-			if k == 0 || k == lenStr {
-				return false
-			}
-			continue
-		}
-		// 数字间有空格
-		_, err := strconv.Atoi(strV)
-		if err != nil {
-			return false
-		}
-	}
-	return true
 }
